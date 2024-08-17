0e4c0b0a4dcf1bcca6c1e6ce1ca5c5a3ebc780b8
Auto Logon <autologon@CSED.LAB>
2024-08-16 21:16:49 -0300

    Começo

--- src/main/kotlin/Personagem.kt
+++ src/main/kotlin/Personagem.kt
@@ -2,8 +2,21 @@ class Personagem {
     var pontosDisponiveis = 27
     var Forca = 8
 
-
+    val custosAtributos = mutableMapOf(
+        9 to 1,
+        10 to 2,
+        11 to 3,
+        12 to 4,
+        13 to 5,
+        14 to 7,
+        15 to 9
+    )
     fun distribuirPontos() {
+        println("Escolha uma opção: ")
+        var escolhaUsuario = 0;
+        while (escolhaUsuario != 0) {
 
+            var a = readLine();
+        }
     }
 }
